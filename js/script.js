35b9f269c975c7cee788817fffbe959b92d65a24
Deltafortal <aldolucchetta21@gmail.com>
2023-06-09 16:08:58 +0200

    Added countdown and random numbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log ('JS OK')
+
+// Prendi tutti gli elementi necessari dal DOM
+const timerPlaceHolder = document.getElementById('timer')
+const numbersList = document.getElementById('numbers')
+
+
+
+
+// Definisci le variabili necessarie
+const numberSlots = 5;
+const totalTimer = 10;
+let time = totalTimer;
+let numbers = [];
+
+
+
+// Genera 5 numeri casuali
+
+for (let i = 0; i < 5; i++) {
+
+    const random = Math.floor(Math.random() * 99) + 1;
+    numbers.push(random);
+}
+
+
+// Aggiungi il Countdown
+
+const timer = setInterval(function(){
+    
+    time--
+    if(time <= 0) {
+        clearInterval(timer)
+        console.log('stopped')
+    }
+    console.log(time)
+
+}, 1000)
